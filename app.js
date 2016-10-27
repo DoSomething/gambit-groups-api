1226c7afe39c6e3f02dc0e405f4ab55c8444d97f
deadlybutter <thedeadlybutter@gmail.com>
2016-10-27 14:44:49 -0400

    Adds group generation

--- app.js
+++ app.js
@@ -31,12 +31,33 @@ app.post('/api/v1/group', function (req, res) {
   const campaignId = req.body.campaign_id;
   const campaignRunId = req.body.campaign_run_id;
 
-  // Check if it exists already in DB
+  // Check if it exists already in db
+  // If it doesn't...
 
-  mobilecommons.createGroup(util.keyGen(campaignId, campaignRunId))
-  .then(mc => {
-    console.log(mc);
-    res.send(mc);
+  const keys = environments.map(env => util.groupKeyGen(campaignId, campaignRunId, env));
+  const groupData = {local: '', thor: '', production: ''};
+
+  function updateGroupData(res, env) {
+    if (!res || !res.response.group) {
+      console.log(res);
+      return;
+    }
+
+    const group = res.response.group;
+    const id = group[0]['$'].id;
+    groupData[env] = id;
+  }
+
+  // There is probably a dynamic way of doing this, but this works for now.
+  mobilecommons.createGroup(keys[environments.indexOf('local')])
+  .then(mcRes => updateGroupData(mcRes, 'local'))
+  .then(() => mobilecommons.createGroup(keys[environments.indexOf('thor')]))
+  .then(mcRes => updateGroupData(mcRes, 'thor'))
+  .then(() => mobilecommons.createGroup(keys[environments.indexOf('production')]))
+  .then(mcRes => updateGroupData(mcRes, 'production'))
+  .then(() => {
+    // Save to db
+    res.json(groupData);
   })
 });
 
