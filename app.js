ebee6c87f38d40730907eb9674a76a16f0f8d5b9
Aaron Schachter <aaronschachter@gmail.com>
2017-04-04 13:45:54 -0700

    Adds winston for debugging

--- app.js
+++ app.js
@@ -4,13 +4,29 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+const logger = require('winston');
+logger.configure({
+  transports: [
+    new (logger.transports.Console)({
+      prettyPrint: true,
+      colorize: true,
+      level: process.env.LOGGING_LEVEL || 'info',
+      timestamp: true,
+    }),
+  ],
+});
+
 const apiRouter = express.Router();
 const api = require('./lib/routes/api')(apiRouter);
 app.use('/api/v1', apiRouter);
 
+app.get('/', function (req, res) {
+  res.json({ status: 'ok' });
+});
+
 const port = process.env.PORT;
 app.listen(port, function () {
-  console.log(`Messaging Groups API listening on port ${port}`);
+  logger.info(`Messaging Groups API listening on port ${port}`);
 });
 
 module.exports = app;
